b346d5054f1589e04158415e79b280578cfc887f
Priti Bhutiya <168433293+PritiBhutiya@users.noreply.github.com>
2026-03-19 16:06:39 +0000

    conversion of datatypes

--- 01_basics/03_converstionOperation.js
+++ 01_basics/03_converstionOperation.js
@@ -1,9 +1,27 @@
-let score = 33
+let score = true
 
 // console.log(typeof score);
 // console.log(typeof(score)); 
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber));
+// console.log(typeof(score));
+// console.log(valueInNumber);
 
-console.log(valueInNumber);
+// "33" => 33
+// "33abs" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = "priti"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=>true, 0=>false
+// ""=>false, string=>true
+
+let someNumber = 33
+
+let stingSomeNumber = String(someNumber)
+
+console.log(typeof someNumber);
+console.log(typeof stingSomeNumber);
